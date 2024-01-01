558680ee6e682fabb6f2508ee5d535f3bdcfd323
SoumyajitGhosh <sjittorres@gmail.com>
2024-01-01 17:57:54 +0530

    Sanity Schemas added

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,1 +1,18 @@
-export const schemaTypes = []
+import {aboutus} from './aboutus'
+import {brands} from './brands'
+import {contact} from './contact'
+import {experiences} from './experiences'
+import {workExperience} from './workExperience'
+import {works} from './works'
+import {skills} from './skills'
+import {testimonials} from './testimonials'
+export const schemaTypes = [
+  aboutus,
+  brands,
+  contact,
+  experiences,
+  skills,
+  testimonials,
+  workExperience,
+  works,
+]
