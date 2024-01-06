f337fd6aebb59d4cb0d0752a296ab159e20d25be
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-01-07 00:54:26 +0530

    chore: All the additional structures have been created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.scss";
-import { Navbar} from "./components";
+import { Navbar } from "./components";
+import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Skills from "./components/Skills/Skills";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Work from "./components/Work/Work";
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar />
       {Header}
+      {About}
+      {Work}
+      {Skills}
+      {Testimonial}
+      {Footer}
     </div>
   );
 }
